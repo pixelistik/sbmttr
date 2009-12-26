b773ca10c4425418f13c8cc8dc14071bb4bf5d0c
Till <code@pixelistik.de>
2009-12-26 01:40:43 +0100

    Remove EMERGEANDSEE strings from config

--- app/config/submittr.php
+++ app/config/submittr.php
@@ -1,7 +1,7 @@
 <?php
 // Maximum size for user uploaded pictures (bytes):
 $config['max_picture_size']=524288;
-$config['organisation_name']='EMERGEANDSEE';
-$config['organisation_email_domain']='emergeandsee.org';
-$config['header-title']='EMERGEANDSEE Submission';
+$config['organisation_name']='Sample Organisation';
+$config['organisation_email_domain']='sample.organisation.tld';
+$config['header-title']='Sample Submissions';
 ?>
