b00e91e1a3fdeeea4aa736d9883147aa4c79badc
Till <code@pixelistik.de>
2010-01-03 02:15:33 +0100

    Add list of accepted extensions for images, documents and videos

--- app/config/sbmttr.php
+++ app/config/sbmttr.php
@@ -4,4 +4,23 @@ $config['max_file_upload_size']=524288;
 $config['organisation_name']='Sample Organisation';
 $config['organisation_email_domain']='sample.organisation.tld';
 $config['header-title']='Sample Submissions';
+// Accepted file extensions for different upload types:
+$config['accepted_file_extensions']=array(
+	'image'=>array(
+		'jpg',
+		'jpeg',
+		'gif',
+		'png',
+		'tif',
+		'tiff'
+		),
+	'document'=>array(
+		'pdf',
+		'doc'
+		),
+	'video'=>array(
+		'avi',
+		'mov'
+		)
+	)
 ?>
