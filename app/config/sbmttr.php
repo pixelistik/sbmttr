f3be345054f9ca0dc924946ad25f39416a4cc140
Till <code@pixelistik.de>
2010-01-03 01:55:14 +0100

    Change picture size to generic upload file size limit in config

--- app/config/sbmttr.php
+++ app/config/sbmttr.php
@@ -1,6 +1,6 @@
 <?php
 // Maximum size for user uploaded pictures (bytes):
-$config['max_picture_size']=524288;
+$config['max_file_upload_size']=524288;
 $config['organisation_name']='Sample Organisation';
 $config['organisation_email_domain']='sample.organisation.tld';
 $config['header-title']='Sample Submissions';
