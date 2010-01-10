5a5c26a9344ca78f707c515a620c884f9ad9316a
Till <code@pixelistik.de>
2010-01-10 01:02:10 +0100

    Upload model can now return file path

--- app/models/upload.php
+++ app/models/upload.php
@@ -13,6 +13,13 @@ class Upload extends AppModel {
 			'order' => ''
 		)
 	);
+	
+	function getFilePath($id){
+		$upload=$this->findById($id);
+		$uploadFolder=APP.'..'.DS.'uploads'.DS.sprintf('%05d',$upload['Upload']['piece_id']).DS;
+		$path=sprintf('%s%05d.%s',$uploadFolder,$upload['Upload']['id'],$upload['Upload']['extension']);
+		return($path);
+	}
 
 }
 ?>
