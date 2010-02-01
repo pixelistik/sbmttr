114d8f7236b6842e8703355b40868c2d44d84b1f
Till <code@pixelistik.de>
2010-02-01 23:07:11 +0100

    Differentiate between http and ftp maximum file size in config

--- app/config/sbmttr.php
+++ app/config/sbmttr.php
@@ -1,9 +1,11 @@
 <?php
 // Maximum size for user uploaded pictures (bytes):
-$config['max_file_upload_size']=524288;
 $config['organisation_name']='Sample Organisation';
 $config['organisation_email_domain']='sample.organisation.tld';
 $config['header-title']='Sample Submissions';
+$config['max_http_upload_size']=524288;
+$config['max_file_upload_size']=524288; //deprecated
+$config['max_ftp_upload_size']=1073741824; // 1GB
 $config['ftp-hostname']='server.test';
 $config['ftp-upload-root']=APP.'..'.DS.'ftp-uploads-tmp'.DS;
 $config['upload-storage-root']=APP.'..'.DS.'uploads'.DS;
